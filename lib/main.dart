e2dac684abbf35020ae6c1b770035dc103583484
Bjarne Tvedten <bjarne.tvedten@apply.no>
2025-01-03 18:09:32 +0100

    Setup the navigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:circle_nav_bar/circle_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,13 +8,96 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  int _tabIndex = 1;
+  int get tabIndex => _tabIndex;
+  set tabIndex(int v) {
+    _tabIndex = v;
+    setState(() {});
+  }
+
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: _tabIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: CircleNavBar(
+        activeIcons: const [
+          Icon(Icons.attach_money_rounded, color: Colors.white),
+          Icon(Icons.home, color: Colors.white),
+          Icon(Icons.info, color: Colors.white),
+        ],
+        inactiveIcons: const [
+          Text("Coins", style: TextStyle(color: Colors.white)),
+          Text("Home", style: TextStyle(color: Colors.white)),
+          Text("About", style: TextStyle(color: Colors.white)),
+        ],
+        color: Colors.deepPurple,
+        height: 60,
+        circleWidth: 60,
+        activeIndex: tabIndex,
+        onTap: (index) {
+          tabIndex = index;
+          pageController.jumpToPage(tabIndex);
+        },
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+        cornerRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(24),
+          bottomLeft: Radius.circular(24),
         ),
+        shadowColor: Colors.deepPurple,
+        elevation: 10,
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (v) {
+          tabIndex = v;
+        },
+        children: [
+          Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.pink),
+          Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.black),
+          Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.red),
+        ],
       ),
     );
   }
